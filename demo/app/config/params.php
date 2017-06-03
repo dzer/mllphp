83b7102b0866a64c1cac47e1a10e9ae9b2c38213
dzer <358654744@qq.com>
2017-06-04 02:02:48 +0800

    index

--- demo/app/config/params.php
+++ demo/app/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'param' => [
+    'params' => [
         'hehe' => 'dfdf'
     ]
 ];
--- demo/public/index.php
+++ demo/public/index.php
@@ -1,4 +1,9 @@
 <?php
-use mll\Mll;
-require '../../vendor/autoload.php';
-Mll::run();
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../app/');
+//调试模式
+define('MLL_DEBUG', true);
+
+// 加载框架引导文件
+require __DIR__ . '/../../vendor/dzer/mll-framework/main.php';
