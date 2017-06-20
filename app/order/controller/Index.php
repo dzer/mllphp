a57b8a60585ad473bb4817b64d1ba9e42f1dd86f
dzer <358654744@qq.com>
2017-06-20 10:53:15 +0800

    demo

---
+++ app/order/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\index\controller;
+
+use Mll\Controller\Controller;
+use Mll\Core\Container;
+use Mll\Mll;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        //
+        return $this->json(array('error' => 0, 'message' => '成功'));
+    }
+}
